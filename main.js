6a6c4b4f01eedf059826a2566adae0aa034e6a1f
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-01-05 16:33:38 -0700

    Complete saveIdeas section

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var titleInput = document.querySelector('#title');
 var bodyInput = document.querySelector('#body');
 var formSection = document.querySelector('.form-section');
 var saveButton = document.querySelector('.save-button');
+var ideaSection = document.querySelector('.idea-section-grid');
 var ideaArray = [];
 
 formSection.addEventListener('mouseover', checkForm);
@@ -17,7 +18,7 @@ formSection.addEventListener('click', function(event) {
 
 function checkForm() {
   if(!titleInput.value || !bodyInput.value) {
-    saveButton.style.backgroundColor = 'grey';
+    saveButton.style.backgroundColor = '#A9AAD2';
     saveButton.style.cursor = 'not-allowed';
     saveButton.disabled = true;
   } else {
@@ -28,10 +29,29 @@ function checkForm() {
 
 function saveIdea(title, body) {
   var idea = new Idea(title, body);
+  titleInput.value = '';
+  bodyInput.value = '';
   ideaArray.push(idea);
   console.log(ideaArray);
+  displayIdeas();
 }
 
-function convertIdea(idea) {
-
+function displayIdeas() {
+  ideaSection.innerHTML = '';
+  ideaArray.forEach(function(element) {
+    ideaSection.innerHTML += `<div class="card">
+            <div class="card-top">
+              <img src="./assets/star.svg" alt="star"/>
+              <img src="./assets/delete.svg" alt="delete"/>
+            </div>
+            <div class="card-body">
+              <h4>${element.title}</h4>
+              <p>${element.body}</p>
+            </div>
+            <div class='card-comment'>
+              <img src="./assets/comment.svg" alt="comment"/>
+              <p>Comment</p>
+            </div>
+          </div>`
+  })
 }
